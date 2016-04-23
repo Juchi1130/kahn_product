2ba3c9d89bb12ac441f5700e3bdf1ae2847eb090
Juergen <juergen.kahn@me.com>
2016-04-23 20:55:50 +0200

    Kahn: start

---
+++ Product.java
@@ -0,0 +1,21 @@
+
+
+public class Product {
+	private String name;
+	
+	public Product(String name) {
+		setName(name);
+	}
+
+	public Product(){
+		setName("Coca Cola");
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+}
